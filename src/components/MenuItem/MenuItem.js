f70916759869ff30fc2a87708db96672d12c5ba8
plamenyovchev <plamenkooo@gmail.com>
2019-11-13 23:03:51 +0200

    Improve styling of MenuItem components

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -4,12 +4,13 @@ import "./MenuItem.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      className={`menu-item ${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
