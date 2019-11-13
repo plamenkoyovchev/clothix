0028659849564de6844820971c704b21eb4b4eac
plamenyovchev <plamenkooo@gmail.com>
2019-11-13 23:14:11 +0200

    Wrap App.js component with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter basename="/clothix">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
