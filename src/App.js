076233c88dcd5021afeccdcfc462625750dbba2f
plamenyovchev <plamenkooo@gmail.com>
2019-11-13 07:06:41 +0200

    Add HomePage and it's styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div>
-      Hello from clothix!
+      <HomePage />
     </div>
   );
 }
