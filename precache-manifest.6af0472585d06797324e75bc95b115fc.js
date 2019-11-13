11615b2c83a82be4cb3cfcaff8c9f032189fe8b7
plamenyovchev <plamenkooo@gmail.com>
2019-11-13 23:20:44 +0200

    Updates

---
+++ precache-manifest.6af0472585d06797324e75bc95b115fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dce361b95b325990e447ab0e5a13b25",
+    "url": "/clothix/index.html"
+  },
+  {
+    "revision": "74b5b2b445834da8a387",
+    "url": "/clothix/static/css/main.fb229c4d.chunk.css"
+  },
+  {
+    "revision": "11a20ca5060a0b7bd95a",
+    "url": "/clothix/static/js/2.0da32e59.chunk.js"
+  },
+  {
+    "revision": "74b5b2b445834da8a387",
+    "url": "/clothix/static/js/main.b21ca09c.chunk.js"
+  },
+  {
+    "revision": "b926e96ca7e0b1d900aa",
+    "url": "/clothix/static/js/runtime-main.668cec87.js"
+  }
+]);
