d8d2848d536d0beed518bff3697a80b6ec8814b7
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-14 16:31:21 +0200

    Get only 4 items for CollectionsPreview

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -7,7 +7,9 @@ const CollectionPreview = ({ title, items}) => {
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview"></div>
-            {items.map(({id, name}) => (
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({id, name}) => (
                 <div key={id}>{name}</div>
             ))}
         </div>
