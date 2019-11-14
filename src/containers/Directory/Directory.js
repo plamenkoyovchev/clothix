84b0176f2baadfc0612232c51cbb37591ebb9174
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-14 16:28:48 +0200

    Move Directory component to containers folder

--- src/containers/Directory/Directory.js
+++ src/containers/Directory/Directory.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./Directory.scss";
-import MenuItem from "../MenuItem/MenuItem";
+import MenuItem from "../../components/MenuItem/MenuItem";
 
 class Directory extends React.Component {
   constructor(props) {
