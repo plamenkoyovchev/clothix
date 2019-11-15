e6051cc53b437587abe4c6f7f3fd12576f5fe029
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-15 08:43:19 +0200

    SignUp component initial commit

---
+++ src/components/Auth/SignUp/SignUp.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./SignUp.scss";
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div>Sign Up</div>;
+  }
+}
+
+export default SignUp;
