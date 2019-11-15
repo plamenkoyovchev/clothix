9280636cfc327aaa6af4065a88aeace22d68dc4c
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-15 09:26:18 +0200

    Use Input component in SignIn component

--- src/components/Auth/SignIn/SignIn.js
+++ src/components/Auth/SignIn/SignIn.js
@@ -2,13 +2,29 @@ import React, { Component } from "react";
 
 import "./SignIn.scss";
 
+import Input from "../../UI/Input/Input";
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      authForm: {
+        email: {
+          type: "email",
+          name: "email",
+          label: "Email",
+          value: "",
+          required: true
+        },
+        password: {
+          type: "password",
+          name: "password",
+          label: "Password",
+          value: "",
+          required: true
+        }
+      }
     };
   }
 
@@ -21,34 +37,31 @@ class SignIn extends Component {
   handleChangeInput = event => {
     const { name, value } = event.target;
 
+    //TODO: set new form state without mutating the old form
+
     this.setState({ [name]: value });
   };
 
+  getFormInputs = () => {
+    const { authForm } = this.state;
+    const formConfig = [];
+    for (const key in authForm) {
+      formConfig.push({ id: key, ...authForm[key] });
+    }
+
+    return formConfig.map(({ id, ...otherProps }) => (
+      <Input key={id} {...otherProps} changed={this.handleChangeInput} />
+    ));
+  };
+
   render() {
+    const formInputs = this.getFormInputs();
+
     return (
       <div className="SignIn">
         <h3>Sign In if you already have an account</h3>
         <form onSubmit={this.handleFormSubmit}>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              required
-              onChange={this.handleChangeInput}
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              required
-              onChange={this.handleChangeInput}
-            />
-          </div>
+          {formInputs}
           <button type="submit">Sign In</button>
         </form>
       </div>
