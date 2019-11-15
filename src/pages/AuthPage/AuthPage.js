406053ae0f3af55727595edc9288445ecf433e10
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-15 08:43:43 +0200

    Add SignIn component to AuthPage

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./AuthPage.scss";
+import SignIn from "../../components/Auth/SignIn/SignIn";
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 };
 
 export default AuthPage;
