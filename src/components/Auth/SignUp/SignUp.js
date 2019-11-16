b70abce871a3b16c6f5e2e0945b3e8ffce81d414
plamenyovchev <plamenkooo@gmail.com>
2019-11-16 17:29:00 +0200

    Implement SignUp component html and styling

--- src/components/Auth/SignUp/SignUp.js
+++ src/components/Auth/SignUp/SignUp.js
@@ -1,16 +1,84 @@
 import React, { Component } from "react";
 
 import "./SignUp.scss";
+import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      signUpForm: {
+        displayName: {
+          type: "text",
+          name: "displayName",
+          label: "Display Name",
+          value: "",
+          required: true
+        },
+        email: {
+          type: "email",
+          name: "email",
+          label: "Email",
+          value: "",
+          required: true
+        },
+        password: {
+          type: "password",
+          name: "password",
+          label: "Password",
+          value: "",
+          required: true
+        },
+        confirmPassword: {
+          type: "password",
+          name: "confirmPassword",
+          label: "Confirm Password",
+          value: "",
+          required: true
+        }
+      }
+    };
   }
 
+  handleFormSubmit = event => {
+    event.preventDefault();
+  };
+
+  handleInputChange = (event, inputId) => {};
+
+  getFromInputs = () => {
+    const { signUpForm } = this.state;
+    const formConfig = [];
+    for (const key in signUpForm) {
+      formConfig.push({
+        id: key,
+        ...signUpForm[key]
+      });
+    }
+
+    return formConfig.map(({ id, ...otherProps }) => (
+      <Input
+        key={id}
+        {...otherProps}
+        changed={event => this.handleInputChange(event, id)}
+      />
+    ));
+  };
+
   render() {
-    return <div className="SignUp"> Sign Up Component</div>;
+    const formInputs = this.getFromInputs();
+    return (
+      <div className="SignIn">
+        <h2 className="title">I don't have an account</h2>
+        <p className="sub-title">Sign up with email and password</p>
+        <form onSubmit={this.handleFormSubmit}>
+          {formInputs}
+          <Button type="submit">Sign Up</Button>
+        </form>
+      </div>
+    );
   }
 }
 
