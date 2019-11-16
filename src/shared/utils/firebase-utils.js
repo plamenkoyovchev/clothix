defc23f8469838e7c770f03418f6a830e9b316b2
plamenyovchev <plamenkooo@gmail.com>
2019-11-16 14:45:36 +0200

    Add firebase utils

---
+++ src/shared/utils/firebase-utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUlA7CZRYZY4YnYRs_yGMbZLkKPEzwiNI",
+    authDomain: "clothix-9a176.firebaseapp.com",
+    databaseURL: "https://clothix-9a176.firebaseio.com",
+    projectId: "clothix-9a176",
+    storageBucket: "clothix-9a176.appspot.com",
+    messagingSenderId: "1059121015550",
+    appId: "1:1059121015550:web:1ce406731674c78cd393af",
+    measurementId: "G-BQ1CDT8FRF"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(googleAuthProvider);
+
+export default firebase;
