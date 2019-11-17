93016cf5fd02f45176310125fb7134bb43d0af3a
plamenyovchev <plamenkooo@gmail.com>
2019-11-17 15:21:04 +0200

    Add redux Provider & store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter basename="/clothix/#">
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename="/clothix/#">
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
