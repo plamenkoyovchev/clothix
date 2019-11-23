f26c75a50c5af9ab2e3f2434c1e265d92314f0b8
plamenyovchev <plamenkooo@gmail.com>
2019-11-23 09:27:53 +0200

    Get correct items in cart quantity

--- src/components/ShoppingCart/CartIcon/CartIcon.jsx
+++ src/components/ShoppingCart/CartIcon/CartIcon.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingBag } from "../../../assets/images/shopping-bag.svg";
 
 import "./CartIcon.scss";
 
-const CartIcon = ({ clicked }) => {
+const CartIcon = ({ clicked, quantity }) => {
   return (
     <div className="cart-icon" onClick={clicked}>
       <ShoppingBag className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{quantity}</span>
     </div>
   );
 };
 
-export default CartIcon;
+const mapStateToProps = state => {
+  return {
+    quantity: state.cart.cartItems.length
+  };
+};
+
+export default connect(mapStateToProps)(CartIcon);
