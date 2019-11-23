b3d9954ea9a7a66e7cb2e94a34d95fd199e858d4
plamenyovchev <plamenkooo@gmail.com>
2019-11-23 20:27:56 +0200

    Add CartItem component to Cart

--- src/components/ShoppingCart/Cart/Cart.jsx
+++ src/components/ShoppingCart/Cart/Cart.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
 import "./Cart.scss";
+import CartItem from "./CartItem/CartItem";
+
+import { connect } from "react-redux";
 
 import Button from "../../UI/Button/Button";
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
   return (
     <div className="Cart">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(({ id, ...otherProps }) => (
+          <CartItem key={id} {...otherProps} />
+        ))}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = state => {
+  return {
+    cartItems: state.cart.cartItems
+  };
+};
+
+export default connect(mapStateToProps)(Cart);
