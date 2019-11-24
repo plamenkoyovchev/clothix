f175d9322aed0c94e6080abf9b2cc2f3333a6936
plamenyovchev <plamenkooo@gmail.com>
2019-11-24 19:51:16 +0200

    Refactor Directory component to functional component and get state from directory selector

--- src/containers/Directory/Directory.jsx
+++ src/containers/Directory/Directory.jsx
@@ -1,59 +1,24 @@
 import React from "react";
 
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { getDirectoryItemsSelector } from "../../store/selectors/directorySelector";
+
 import "./Directory.scss";
 import MenuItem from "../../components/MenuItem/MenuItem";
 
-class Directory extends React.Component {
-  constructor(props) {
-    super(props);
+const Directory = ({ items }) => {
+  return (
+    <div className="directory-menu">
+      {items.map(({ id, ...otherItemsProps }) => (
+        <MenuItem key={id} {...otherItemsProps} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      items: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats"
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets"
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers"
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens"
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens"
-        }
-      ]
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  items: getDirectoryItemsSelector
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.items.map(({ id, ...otherItemsProps }) => (
-          <MenuItem key={id} {...otherItemsProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
