1fac3ce8e51e3583b4cbe375bc06f2cec0c326bd
plamenyovchev <plamenkooo@gmail.com>
2019-11-24 19:39:02 +0200

    Add shop collections into redux and implement selector to get the data

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,10 +4,12 @@ import {
 
 import userReducer from './user';
 import cartReducer from './cart';
+import shopReducer from './shop';
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    shop: shopReducer
 });
 
 export default rootReducer;
