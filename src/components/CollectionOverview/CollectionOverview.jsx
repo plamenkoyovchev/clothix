facf014499d814c4508327e951a3c21f1b2a184f
plamenyovchev <plamenkooo@gmail.com>
2019-11-25 23:01:20 +0200

    Implement CollectionOverview component

---
+++ src/components/CollectionOverview/CollectionOverview.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./CollectionOverview.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { getShopCollectionsForPreview } from "../../store/selectors/shopSelector.js";
+
+import CollectionPreview from "../CollectionPreview/CollectionPreview";
+
+const CollectionOverview = ({ collections }) => {
+  return (
+    <div className="CollectionOverview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: getShopCollectionsForPreview
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
