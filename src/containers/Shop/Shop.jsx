a9c5021ac9c719585fd1b42d0fdda91418a7a2f2
plamenyovchev <plamenkooo@gmail.com>
2019-11-25 23:01:34 +0200

    Add CollectionOverview in Shop

--- src/containers/Shop/Shop.jsx
+++ src/containers/Shop/Shop.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 
 import "./Shop.scss";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { getShopCollectionsForPreview } from "../../store/selectors/shopSelector";
 
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
+import CollectionOverview from "../../components/CollectionOverview/CollectionOverview";
 
-const Shop = ({ collections }) => {
+const Shop = () => {
   return (
     <div className="Shop">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: getShopCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
