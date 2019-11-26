97778df5c34203d0d0b95b918799b2a6fe7c6e99
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-26 08:54:03 +0200

    Register Route for Collection component

--- src/containers/Shop/Shop.jsx
+++ src/containers/Shop/Shop.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Shop.scss";
 import { Route } from "react-router-dom";
+
 import CollectionOverview from "../../components/CollectionOverview/CollectionOverview";
+import Collection from "../../components/CollectionOverview/Collection/Collection";
 
 const Shop = ({ match }) => {
   return (
     <div className="Shop">
       <Route exact path={match.path} component={CollectionOverview} />
-      <Route path={`${match.path}/category`} component={CollectionOverview} />
+      <Route path={`${match.path}/:category`} component={Collection} />
     </div>
   );
 };
