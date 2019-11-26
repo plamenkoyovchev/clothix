c09232a09133df388b40d8dc15954cc0144c2922
plamenyovchev <plamenkooo@gmail.com>
2019-11-26 21:43:25 +0200

    Refactor CollectionOverview to styled component

--- src/components/CollectionOverview/CollectionOverview.jsx
+++ src/components/CollectionOverview/CollectionOverview.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-import "./CollectionOverview.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { getShopCollectionsForPreview } from "../../store/selectors/shopSelector.js";
 
+import { CollectionOverviewContainer } from "./CollectionOverview.styles";
 import CollectionPreview from "./CollectionPreview/CollectionPreview";
 
 const CollectionOverview = ({ collections }) => {
   return (
-    <div className="CollectionOverview">
+    <CollectionOverviewContainer className="CollectionOverview">
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </CollectionOverviewContainer>
   );
 };
 
