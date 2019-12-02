60f698d5f7ef26ef2e0cf4c175baacdd6b93a166
plamenyovchev <plamenkooo@gmail.com>
2019-12-02 21:14:42 +0200

    Implement method to add collection and documents to firebase

--- src/shared/utils/firebase-utils.js
+++ src/shared/utils/firebase-utils.js
@@ -47,4 +47,15 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 }
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    const batch = firestore.batch();
+    objectsToAdd.forEach(item => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, item);
+    });
+
+    await batch.commit();
+}
+
 export default firebase;
