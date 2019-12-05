3f9458ef8130cbe6c83deb440c9425aa40edcd76
plamenyovchev <plamenkooo@gmail.com>
2019-12-05 07:55:54 +0200

    Register redux-thunk into app store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { persistStore } from "redux-persist";
 
 import rootReducer from './reducers/rootReducer';
 
-const middleware = [logger];
+const middleware = [thunk, logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 const persistor = persistStore(store);
